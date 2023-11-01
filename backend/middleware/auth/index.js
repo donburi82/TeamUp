84a57b0aab40a0f82ef33d50780857f3a5aea6dc
Samson <cwyeung114@gmail.com>
2023-11-02 00:50:26 +0800

    bug fix on route

---
+++ backend/middleware/auth/index.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  // check header
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.json({ msg: "Authentication invalid" }).status(401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: payload.userId, role: payload.role };
+    next();
+  } catch (error) {
+    return res.json({ msg: "Authentication invalid" }).status(401);
+  }
+};
+
+module.exports = auth;
--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -5,8 +5,10 @@ const User = require("../../models/users");
 const express = require("express");
 const router = express.Router();
 
-router.route("/auth/verify").get(async (req, res) => {
-  const verifyCode = _.random(100000, 999999);
+let verificationCodes = {};
+
+router.route("/verification").get(async (req, res) => {
+  const verificatoinCode = _.random(100000, 999999);
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -24,7 +26,7 @@ router.route("/auth/verify").get(async (req, res) => {
               <br/>
               <p>Thank you for choosing TeamUp.</p>
               <p>To verify your account, please enter the following code in the app:</p>
-              <p><strong>${verifyCode}</strong></p>
+              <p><strong>${verificatoinCode}</strong></p>
               <p>This code will expire in 10 minutes.</p>
               <p>Please do not reply to this email. This mailbox is not monitored and you will not receive a response.</p>
               <br/>
@@ -39,12 +41,29 @@ router.route("/auth/verify").get(async (req, res) => {
       res.status(500).send({ msg: "Internal Server Error" });
     } else {
       console.log("Email sent: " + info.response);
-      res.status(200).send({ verifyCode });
+      verificationCodes[req.body.email] = verificatoinCode;
+      console.log(verificationCodes);
+      setTimeout(() => {
+        delete verificationCodes[req.body.email];
+        console.log(verificationCodes);
+      }, 1000 * 60 * 5);
+      res.status(200).send({ status: "success" });
     }
   });
 });
 
-router.route("/auth/register").post(async (req, res) => {
+router.route("/verify").get(async (req, res) => {
+  if (verificationCodes[req.body.email] == req.body.verificationCode) {
+    console.log(verificationCodes);
+    delete verificationCodes[req.body.email];
+    console.log(verificationCodes);
+    res.status(200).send({ status: "success" });
+  } else {
+    res.status(401).send({ status: "incorrect credentials" });
+  }
+});
+
+router.route("/register").post(async (req, res) => {
   try {
     const user = await User.create({
       email: req.body.email,
@@ -57,22 +76,25 @@ router.route("/auth/register").post(async (req, res) => {
       res.status(409).json({
         err: `email has already be taken.`,
       });
+    } else {
+      res.json({ error });
     }
   }
 });
 
-router.route("/auth/login").post(async (req, res) => {
+router.route("/login").post(async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ msg: "email don't exit" });
+      return res.status(400).json({ msg: "incorrect credentials" });
     }
 
     const isPasswordCorrect = await user.comparePassword(password);
     if (!isPasswordCorrect) {
-      return res.status(401).json({ msg: "incorrect password" });
+      return res.status(401).json({ msg: "incorrect credentials" });
     }
+
     const token = user.createJWT();
 
     return res.json({ msg: "login successful", token }).status(200);
