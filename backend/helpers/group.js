f3c8733486428d47fa2e51e2506f8db680343dd8
donburi82 <104419950+donburi82@users.noreply.github.com>
2024-02-12 21:44:30 -0600

    implemented helper functions and routes for groups

--- backend/helpers/group.js
+++ backend/helpers/group.js
@@ -1,10 +1,31 @@
 const ObjectId = require('mongodb').ObjectId;
 const { User } = require("../models/user");
 const { Group } = require("../models/group");
+const user = require('../models/user');
 
-async function createGroup(userId, category, project, quota, members) {
+async function getGroups(userId, mode) {
     try {
-        // leader
+        const user = await User.findOne({ _id: new ObjectId(userId) });
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        let groups;
+
+        if (mode=="my") {
+            groups = user.groups;
+        } else if (mode=="available") {
+            console.log("Matching Engine");
+        }
+        
+        return groups;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function createGroup(userId, category, project, quota) {
+    try {
         const user = await User.findOne({ _id: new ObjectId(userId) });
         if (!user) {
             throw new Error("User not found");
@@ -16,9 +37,11 @@ async function createGroup(userId, category, project, quota, members) {
             project: project,
             // projectPeriod
             quota: quota,
-            members: members,
+            members: [userId],
         });
-        // might have to push groupID's to users' groups individually
+
+        user.groups.push(group._id);
+        await user.save();
         
         return await group.save();
     } catch (error) {
@@ -26,6 +49,129 @@ async function createGroup(userId, category, project, quota, members) {
     }
 }
 
+async function deleteGroup(userId, groupId) {
+    try {
+        const group = await Group.findOne({ _id: new ObjectId(groupId) });
+        if (!group) {
+            throw new Error("Group not found");
+        }
+
+        // check whether it's the leader calling the endpoint
+        if (group.leaderID.toString()!==userId.toString()) {
+            throw new Error("Only the leader can delete the group");
+        }
+
+        // update members' group lists
+        await User.updateMany(
+            { _id: { $in: group.members }},
+            { $pull: { groups: groupId }},
+        );
+
+        return await group.deleteOne({ _id: new ObjectId(groupId) });
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function finalizeGroup(userId, groupId) {
+    try {
+        const group = await Group.findOne({ _id: new ObjectId(groupId) });
+        if (!group) {
+            throw new Error("Group not found");
+        }
+
+        // check whether it's the leader calling the endpoint
+        if (group.leaderID.toString()!==userId.toString()) {
+            throw new Error("Only the leader can delete the group");
+        }
+
+        // check whether it has already been finalized or not
+        if (group.finalized===true) {
+            throw new Error("Group already finalized");
+        }
+
+        group.finalized = true;
+        return await group.save();
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function addMembers(userId, groupId, members) {
+    try {
+        const group = await Group.findOne({ _id: new ObjectId(groupId) });
+        if (!group) {
+            throw new Error("Group not found");
+        }
+
+        // check whether it's the leader calling the endpoint
+        if (group.leaderID.toString()!==userId.toString()) {
+            throw new Error("Only the leader can add members");
+        }
+
+        // check whether it has already been finalized or not
+        if (group.finalized===true) {
+            throw new Error("Group already finalized");
+        }
+
+        // need to check it doesn't exceed quota after add
+
+        // add members to the group
+        members.forEach(member => {
+            if (!group.members.includes(member)) {
+                group.members.push(member);
+            }
+        });
+        await group.save();
+
+        // update membership information for every member
+        await User.updateMany(
+            { _id: { $in: members} },
+            { $addToSet: { groups: groupId } }
+        );
+
+        return;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function removeMembers(userId, groupId, members) {
+    try {
+        const group = await Group.findOne({ _id: new ObjectId(groupId) });
+        if (!group) {
+            throw new Error("Group not found");
+        }
+
+        // check whether it's the leader calling the endpoint
+        if (group.leaderID.toString()!==userId.toString()) {
+            throw new Error("Only the leader can remove members");
+        }
+
+        // check whether it has already been finalized or not
+        if (group.finalized===true) {
+            throw new Error("Group already finalized");
+        }
+
+        // need to check len>0 after removal
+
+        // remove members from the group
+        group.members = group.members.filter(member => !members.includes(member.toString()));
+        await group.save();
+
+        // update membership information for every member
+        await User.updateMany(
+            { _id: { $in: members} },
+            { $pull: { groups: groupId } }
+        );
+
+        return;
+    } catch (error) {
+        throw error;
+    }
+}
+
 module.exports = {
-    createGroup
+    getGroups, createGroup, deleteGroup, finalizeGroup,
+    addMembers, removeMembers,
 };
--- backend/routes/groups/index.js
+++ backend/routes/groups/index.js
@@ -2,8 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    createGroup
+    getGroups, createGroup, deleteGroup, finalizeGroup,
+    addMembers, removeMembers,
 } = require("../../helpers/group");
+
+router.get("/", async (req, res) => {
+    try {
+        const { userId, mode } = req.query;
+        const groups = await getGroups(userId, mode);
+        console.log(groups);
+        return res.status(200).json({ success: true, data: groups });
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ success: false });
+    }
+});
 
 router.post("/", async (req, res) => {
     try {
@@ -13,7 +26,58 @@ router.post("/", async (req, res) => {
             req.body.category,
             req.body.project,
             req.body.quota,
-            req.body.members
+        );
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ success: false });
+    }
+});
+
+router.delete("/", async (req, res) => {
+    try {
+        const { userId, groupId } = req.query;
+        await deleteGroup(userId, groupId);
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ success: false });
+    }
+});
+
+router.post("/finalize", async (req, res) => {
+    try {
+        const { userId, groupId } = req.query;
+        await finalizeGroup(userId, groupId);
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ success: false });
+    }
+});
+
+router.post("/members", async (req, res) => {
+    try {
+        console.log(req.body);
+        await addMembers(
+            req.body.userId,
+            req.body.groupId,
+            req.body.members,
+        );
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ success: false });
+    }
+});
+
+router.delete("/members", async (req, res) => {
+    try {
+        console.log(req.body);
+        await removeMembers(
+            req.body.userId,
+            req.body.groupId,
+            req.body.members,
         );
         return res.status(200).json({ success: true });
     } catch (error) {
