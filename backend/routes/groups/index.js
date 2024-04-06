32dc21e28404465c2837a41d5e6f4aaed3d1bfcc
donburi82 <104419950+donburi82@users.noreply.github.com>
2024-04-05 20:32:16 -0500

    added and modfiied group apis for better integration, use user token instead of parameter

--- backend/routes/groups/index.js
+++ backend/routes/groups/index.js
@@ -1,105 +1,123 @@
 const express = require("express");
 const router = express.Router();
 
+const { User } = require("../../models/user");
 const {
-    getMyGroups, getAvailableGroups, createGroup, deleteGroup,
+    getGroupInfo,
+    getMyGroups, getAvailableGroups, createGroup, updateGroup, deleteGroup,
     finalizeGroup, leaveGroup,
     addMembers, removeMembers,
 } = require("../../helpers/group");
 
+router.get("/info", async (req, res) => {
+    try {
+        const { groupId } = req.query;
+        const group = await getGroupInfo(groupId);
+        return res.status(200).json({ success: true, data: group });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ success: false, error: error });
+    }
+});
+
 router.get("/", async (req, res) => {
     try {
-        const { userId, mode } = req.query;
+        const userId = req.user.userId;
+        const { mode } = req.query;
         let groups;
         if (mode=="my") {
-            groups = getMyGroups(userId);
+            groups = await getMyGroups(userId);
         } else if (mode=="available") {
-            groups = getAvailableGroups(userId);
+            groups = await getAvailableGroups(userId);
         }
-        console.log(groups);
         return res.status(200).json({ success: true, data: groups });
     } catch (error) {
         console.log(error);
-        return res.status(401).json({ success: false });
+        return res.status(400).json({ success: false, error: error });
     }
 });
 
 router.post("/", async (req, res) => {
     try {
-        console.log(req.body);
-        await createGroup(
-            req.body.userId,
-            req.body.category,
-            req.body.project,
-            req.body.quota,
-        );
+        const leaderId = req.user.userId;
+        const { name, category, project, quota, members } = req.body;
+        const chatRoom = await createGroup(leaderId, name, category, project, quota, members);
+        return res.status(200).json({ success: true, room: chatRoom._id });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ success: false, error: error });
+    }
+});
+
+router.patch("/", async (req, res) => {
+    try {
+        const userId = req.user.userId;
+        const { groupId, name, category, project, quota } = req.query;
+        await updateGroup(userId, groupId, name, category, project, quota);
         return res.status(200).json({ success: true });
     } catch (error) {
         console.log(error);
-        return res.status(401).json({ success: false });
+        return res.status(400).json({ success: false, error: error });
     }
 });
 
 router.delete("/", async (req, res) => {
     try {
-        const { userId, groupId } = req.query;
+        const userId = req.user.userId;
+        const { groupId } = req.query;
         await deleteGroup(userId, groupId);
         return res.status(200).json({ success: true });
     } catch (error) {
         console.log(error);
-        return res.status(401).json({ success: false });
+        return res.status(400).json({ success: false, error: error });
     }
 });
 
 router.post("/finalize", async (req, res) => {
     try {
-        const { userId, groupId } = req.query;
+        const userId = req.user.userId;
+        const { groupId } = req.query;
         await finalizeGroup(userId, groupId);
         return res.status(200).json({ success: true });
     } catch (error) {
         console.log(error);
-        return res.status(401).json({ success: false });
+        return res.status(400).json({ success: false, error: error });
     }
 });
 
 router.post("/leave", async (req, res) => {
     try {
-        const { userId, groupId } = req.query;
+        const userId = req.user.userId;
+        const { groupId } = req.query;
         await leaveGroup(userId, groupId);
         return res.status(200).json({ success: true });
     } catch (error) {
         console.log(error);
-        return res.status(401).json({ success: false });
+        return res.status(400).json({ success: false, error: error });
     }
 });
 
 router.post("/members", async (req, res) => {
     try {
-        console.log(req.body);
-        await addMembers(
-            req.body.userId,
-            req.body.groupId,
-            req.body.members,
-        );
+        const userId = req.user.userId;
+        const {groupId, members} = req.body;
+        await addMembers(userId, groupId, members);
         return res.status(200).json({ success: true });
     } catch (error) {
         console.log(error);
-        return res.status(401).json({ success: false });
+        return res.status(400).json({ success: false, error: error });
     }
 });
 
 router.delete("/members", async (req, res) => {
     try {
-        console.log(req.body);
-        await removeMembers(
-            req.body.userId,
-            req.body.groupId,
-            req.body.members,
-        );
+        const userId = req.user.userId;
+        const {groupId, members} = req.body;
+        await removeMembers(userId, groupId, members);
         return res.status(200).json({ success: true });
     } catch (error) {
         console.log(error);
-        return res.status(401).json({ success: false });
+        return res.status(400).json({ success: false, error: error });
     }
 });
 
